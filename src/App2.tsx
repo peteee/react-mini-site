a70d9bef6262d75879f0460b3d0049fcb3c63d63
Peter Oberlaender <pete@wayoutofline.net>
2024-03-04 12:40:27 -0500

    src folder

---
+++ src/App2.tsx
@@ -0,0 +1,21 @@
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
+import './App.css'
+
+function App2() {
+
+  return (
+    <>
+      <Header />
+      <main>
+        <h2>Contact me</h2>
+        <p><a href="mailto:kkjshdf#@pizza.qc.ca">Send us an Email</a></p>
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default App2
