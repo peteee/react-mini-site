01dc7e4b5504590189b5f04759694e7630e967e7
Peter Oberlaender <pete@wayoutofline.net>
2024-03-04 12:42:23 -0500

    components folder

---
+++ src/components/Main.tsx
@@ -0,0 +1,15 @@
+function Main() {
+    return (
+      <>
+        <main>
+            <h2>Under contruction</h2>
+        </main>
+        <section id="about">
+          <h3>About</h3>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit quidem vitae optio quasi excepturi facere, enim obcaecati totam ab harum adipisci deleniti illum necessitatibus itaque, atque voluptatum ipsam nihil beatae.
+        </section>
+      </>
+    );  
+  }
+  
+export default Main
---
+++ src/components/Menu.tsx
@@ -0,0 +1,12 @@
+function Menu() {
+    return (
+        <ul>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="index.html#about">About</a></li>
+            <li><a href="contact.html">Contact</a></li>
+        </ul>
+
+    );  
+  }
+  
+export default Menu
