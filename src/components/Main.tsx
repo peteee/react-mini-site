dca539e06d39c1f2857a2a1f36b1dcdaa2d767b8
Peter Oberlaender <pete@wayoutofline.net>
2024-03-18 14:09:00 -0400

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,6 +8,11 @@ function Main() {
           <h3>About</h3>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Suscipit quidem vitae optio quasi excepturi facere, enim obcaecati totam ab harum adipisci deleniti illum necessitatibus itaque, atque voluptatum ipsam nihil beatae.
         </section>
+        <section id="contact">
+          <h3>Get in touch</h3>
+          <p><a href="mailto:lhasdkjhasd@gmail.com">Email</a></p>
+          <p><a href="tel:+15142123123123">Phone</a></p>
+        </section>
       </>
     );  
   }
