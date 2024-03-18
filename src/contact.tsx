2bffab8623b83f485c86129b1fa4796dfeea6f16
Peter Oberlaender <pete@wayoutofline.net>
2024-03-18 14:08:16 -0400

    Delete src/contact.tsx

--- src/contact.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App2 from './App2.tsx'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App2 />
-  </React.StrictMode>
-)
